dad316d7318f1b411dd67277e7f702a14a9a590b
katalyzator <web.coder96@gmail.com>
2016-08-22 15:21:24 +0600

    template added

--- sendmail/views.py
+++ sendmail/views.py
@@ -1,14 +1,29 @@
+import os
+
 from django.core.mail import EmailMessage
-from django.http import HttpResponse
+from django.template import Context, Template
 from django.http import JsonResponse
 from django.shortcuts import render
 
 
 # Create your views here.
+from balastanServer.settings import BASE_DIR
+
 
 def sendMail(request):
     try:
-        email = EmailMessage('title', 'body', to=['aibek.widgets@gmail.com'])
+
+        name = request.POST.get('name')
+        number = request.POST.get('number')
+        f = open(os.path.join(BASE_DIR, "templates/mail.html"))
+
+        content = f.read()
+        f.close()
+        context = Context(dict(name=name, number=number))
+        template = Template(content)
+
+        email = EmailMessage('Balastan', template.render(context), to=['aibek.widgets@gmail.com'])
+        email.content_subtype='html'
         email.send()
         return JsonResponse(dict(result='okay'))
 
