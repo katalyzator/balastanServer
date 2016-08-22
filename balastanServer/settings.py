c4379613488a5c1052024f1c2bdac3f01e4432c8
katalyzator <web.coder96@gmail.com>
2016-08-22 15:55:11 +0600

    change email user

--- balastanServer/settings.py
+++ balastanServer/settings.py
@@ -28,8 +28,8 @@ ALLOWED_HOSTS = []
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'aibek.widgets@gmail.com'
-EMAIL_HOST_PASSWORD = 'izpodkaptal96'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'password_here'
 EMAIL_PORT = 587
 
 # Application definition
